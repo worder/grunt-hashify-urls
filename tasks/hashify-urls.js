d4908272ec3047d9967edc49c07e0db23242f7c5
Marvin Tam <marvintam@gmail.com>
2014-06-09 19:11:42 -0400

    Implement Grunt task

---
+++ tasks/hashify-urls.js
@@ -0,0 +1,30 @@
+'use strict';
+
+var hashifyUrls = require('./lib/hashify-urls.js');
+
+module.exports = function (grunt) {
+  grunt.registerMultiTask('hashifyUrls', 'Hashify CSS url()’s.', function () {
+    var options = this.options;
+
+    // Iterate over all src-dest file pairs.
+    this.files.forEach(function (f) {
+      // Hashify CSS files
+      f.src.filter(function (filepath) {
+        // Warn on and remove invalid source files (if nonull was set).
+        if (!grunt.file.exists(filepath)) {
+          grunt.log.warn('Source file "' + filepath + '" not found.');
+          return false;
+        } else {
+          return true;
+        }
+      }).map(function (filepath) {
+        // Read file source.
+        var content = grunt.file.read(filepath);
+        var hashified = hashifyUrls(content, options);
+
+        grunt.file.write(filepath, hashified);
+        grunt.log.writeln('✔ '.cyan + 'File', filepath.cyan, 'hashified.');
+      });
+    });
+  });
+};
